956e603b8800426cf16be31f0d672a27f25b3327
BodaZsolt <zsolt.boda@straxus.hu>
2021-05-01 13:50:39 +0200

    pretty print

--- riot.py
+++ riot.py
@@ -6,6 +6,7 @@ import sys
 import requests
 from requests import Response
 from typing import Dict
+from pprint import pprint
 
 API_KEY: str = 'RGAPI-3ce3c65a-f561-4dc5-aa50-4b01eb2cd6f0'
 USERNAME: str = 'GTDiablo'
@@ -17,7 +18,7 @@ def main() -> int:
     api_url: str = API_URL.format(region=REGION, version=VERSION, names=USERNAME)
     payload: Dict[str, str] = {'api_key': API_KEY}
     response: Response = requests.get(api_url, params=payload)
-    print(response.json())
+    pprint(response.json())
     return 0
 
 if __name__ == '__main__':
