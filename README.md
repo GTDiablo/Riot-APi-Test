1ee2261206c794798682e48fc3e9d3c1e6cd61ea
BodaZsolt <zsolt.boda@straxus.hu>
2021-05-01 13:45:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Riot API
+
+Ez a script a LOL summoner profil adatait kérdezi le.
+
+## Használat
+
+Telepítés:
+
+    pip install -r requirements.txt
+
+Adj meg minden adatot a riot.py fájl tetején, majd futtasd:
+
+    python riot.py
---
+++ riot.py
@@ -0,0 +1,24 @@
+'''
+'''
+__author__ = 'Boda Zsolt'
+
+import sys
+import requests
+from requests import Response
+from typing import Dict
+
+API_KEY: str = 'RGAPI-3ce3c65a-f561-4dc5-aa50-4b01eb2cd6f0'
+USERNAME: str = 'GTDiablo'
+REGION: str = 'eun1'
+VERSION: int = 4
+API_URL: str = 'https://{region}.api.riotgames.com/lol/summoner/v{version}/summoners/by-name/{names}'
+
+def main() -> int:
+    api_url: str = API_URL.format(region=REGION, version=VERSION, names=USERNAME)
+    payload: Dict[str, str] = {'api_key': API_KEY}
+    response: Response = requests.get(api_url, params=payload)
+    print(response.json())
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
